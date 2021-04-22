5c2ece82f0082e4e6562e04323b55c006926c44e
webmvw <masudrana.bbpi@gmail.com>
2021-04-22 14:23:25 +0600

    shop, cart and checkout component create and template setup

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CheckoutComponent;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,6 @@ use App\Http\Livewire\HomeComponent;
 
 
 Route::get('/', HomeComponent::class);
+Route::get('/shop', ShopComponent::class);
+Route::get('/cart', CartComponent::class);
+Route::get('/checkout', CheckoutComponent::class);
