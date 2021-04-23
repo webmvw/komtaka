6be7f69aad03a23494415c0a61dea68925e5c4ff
webmvw <masudrana.bbpi@gmail.com>
2021-04-23 18:00:04 +0600

    product sorting and product show per page system added

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -11,6 +11,14 @@ class ShopComponent extends Component
 {
 	use WithPagination;
 
+	public $sorting;
+	public $pagesize;
+
+	public function mount(){
+		$this->sorting = "default";
+		$this->pagesize = 12;
+	}
+
 	public function store($product_id, $product_name, $product_price){
 		Cart::add($product_id, $product_name, 1, $product_price)->associate('App\Models\Product');
 		session()->flash('success_message', 'Item added in Cart');
@@ -19,7 +27,15 @@ class ShopComponent extends Component
 
     public function render()
     {
-    	$products = Product::paginate(12);
+    	if($this->sorting == 'date'){
+    		$products = Product::orderBy('created_at', 'desc')->paginate($this->pagesize);
+    	}elseif($this->sorting == 'price'){
+    		$products = Product::orderBy('regular_price', 'asc')->paginate($this->pagesize);
+    	}elseif($this->sorting == 'price-desc'){
+    		$products = Product::orderBy('regular_price', 'desc')->paginate($this->pagesize);
+    	}else{
+    		$products = Product::paginate(12);
+    	}
         return view('livewire.shop-component', ['products'=>$products])->layout('layouts.base');
     }
 }
