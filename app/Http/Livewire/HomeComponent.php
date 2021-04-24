019a7e25a152afff7046c2f09f076e694db204ca
webmvw <masudrana.bbpi@gmail.com>
2021-04-24 22:25:11 +0600

    show latest product into the homepage system added

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -4,12 +4,14 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\HomeSlider;
+use App\Models\Product;
 
 class HomeComponent extends Component
 {
     public function render()
     {
     	$homesliders = HomeSlider::where('status', '1')->get();
-        return view('livewire.home-component', ['homesliders'=>$homesliders])->layout('layouts.base');
+    	$latest_products = Product::orderBy('created_at', 'desc')->get()->take(10);
+        return view('livewire.home-component', ['homesliders'=>$homesliders, 'latest_products'=>$latest_products])->layout('layouts.base');
     }
 }
